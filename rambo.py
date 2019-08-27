3a88a22ab605edfbb8c3fb20113e3c5ac1d7c32d
Lourdunive <54096348+Lourdunive@users.noreply.github.com>
2019-08-27 21:29:52 +0530

    Update rambo.py

--- rambo.py
+++ rambo.py
@@ -12,15 +12,21 @@ uploaded = files.upload()
 data=pd.read_csv('Power-Networks-LCL.csv',parse_dates=["DateTime"],index_col="DateTime")
 data #displays the content of the file
 
+# Find Top3 household that has more samples
+top3 = data.LCLid.value_counts().to_frame().head(3)
+print(top3)
+
 #To plot the Dataset in weekly formate
 week=data.resample('w').sum()
 plt.figure(figsize=(8,5))
 plt.plot(week)
+plt.show()
 
 #plots groupby and mean function for hourly summary
 hr_time=data.groupby(data.index.time).mean()
 hr_ticks=4*60*60* np.arange(6)
 hr_time.plot(xticks=hr_ticks,figsize=(6,5), linewidth=3)
+plt.show()
 
 #Before using Prophet, we rename the columns as "ds" and "y"
 daily=data.resample('D').sum()
@@ -41,6 +47,8 @@ data2_forecast=data2_prophet.predict(data2_forecast)
 #plots the forcasted data
 data2_prophet.plot(data2_forecast,xlabel='Date',ylabel='KWh')
 plt.title('prediction for 4 months ')
+plt.show()
 
 #visualize the overall trend and the component patterns
 data2_prophet.plot_components(data2_forecast)
+plt.show()
